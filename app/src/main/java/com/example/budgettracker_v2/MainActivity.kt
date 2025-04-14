bad2493fe8ac819954fadc90228971d6c24fb67a
Erhan99 <146257442+Erhan99@users.noreply.github.com>
2025-04-14 19:47:46 +0200

    theme, colors and type

--- app/src/main/java/com/example/budgettracker_v2/MainActivity.kt
+++ app/src/main/java/com/example/budgettracker_v2/MainActivity.kt
@@ -6,19 +6,20 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.budgettracker_v2.ui.theme.BudgetTracker_v2Theme
+import com.example.compose.BudgetTrackerTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            BudgetTracker_v2Theme {
+            BudgetTrackerTheme {
 
             }
         }
@@ -29,7 +30,7 @@ class MainActivity : ComponentActivity() {
 @Preview(showBackground = true)
 @Composable
 fun BudgetTrackerPreview() {
-    BudgetTracker_v2Theme {
+    BudgetTrackerTheme() {
 
     }
 }
