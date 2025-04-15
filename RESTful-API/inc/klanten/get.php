a92d431ccb56fd6e4b4fd4748dfbe02e6a633f35
Jorne Van Gils <jorne.vangils@student.odisee.be>
2025-04-15 20:13:21 +0200

    update api

--- RESTful-API/inc/klanten/get.php
+++ RESTful-API/inc/klanten/get.php
@@ -1,6 +1,15 @@
 <?php
 
-$sql="select kl_id, kl_naam, kl_voornaam, kl_email, kl_wachtwoord, kl_isAdmin FROM Klanten";
+$sql = "SELECT 
+			kl_id, kl_naam, kl_voornaam, kl_email, 
+			kl_wachtwoord, kl_isAdmin, 
+			bl_id, bl_inkomsten, bl_uitgaven 
+		FROM 
+			Klanten 
+		JOIN 
+			Balansen 
+		ON 
+			kl_id = bl_kl_id ";
 
 $result = $conn -> query($sql);
 
