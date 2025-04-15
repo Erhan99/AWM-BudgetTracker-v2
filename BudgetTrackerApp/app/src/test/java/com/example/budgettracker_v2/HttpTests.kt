c6e2a5c38cc908bc9c72884c8fe9a4022d5bda40
Erhan99 <146257442+Erhan99@users.noreply.github.com>
2025-04-15 19:22:38 +0200

    api, http methods

--- BudgetTrackerApp/app/src/test/java/com/example/budgettracker_v2/HttpTests.kt
+++ BudgetTrackerApp/app/src/test/java/com/example/budgettracker_v2/HttpTests.kt
@@ -18,10 +18,44 @@ class HttpTests {
     }
 
     @Test
+    fun getTransactiesByUser(){
+        runBlocking {
+            val response = apiTransaction.geTransactiesByUser("2")
+            assertEquals(true, response.isSuccessful)
+        }
+    }
+
+    @Test
     fun postTransactieTest(){
         runBlocking {
             val transaction = Transaction(tr_bedrag = 999.00, tr_mededeling = "testing post", tr_begunstigde = "testing post", tr_dt_id = 1, tr_bl_id = 1, tr_ct_id = 1)
             val response = apiTransaction.postTransacties(transaction)
+            assertEquals(true, response.isSuccessful)
+        }
+    }
+
+    @Test
+    fun updateTransactieTest(){
+        runBlocking {
+            val transaction = Transaction(tr_id = 8 ,tr_bedrag = 999.00, tr_mededeling = "testing post", tr_begunstigde = "testing post", tr_dt_id = 1, tr_bl_id = 1, tr_ct_id = 1)
+            val response = apiTransaction.updateTransacties(transaction)
+            assertEquals(true, response.isSuccessful)
+        }
+    }
+
+    @Test
+    fun updateTransactieZonderIdTest(){
+        runBlocking {
+            val transaction = Transaction(tr_bedrag = 999.00, tr_mededeling = "testing post", tr_begunstigde = "testing post", tr_dt_id = 1, tr_bl_id = 1, tr_ct_id = 1)
+            val response = apiTransaction.updateTransacties(transaction)
+            assertEquals(false, response.isSuccessful)
+        }
+    }
+
+    @Test
+    fun Deletetransactietest(){
+        runBlocking {
+            val response = apiTransaction.deleteTransacties("24")
             assertEquals(true, response.isSuccessful)
         }
     }
--- RESTful-API/inc/transacties/delete.php
+++ RESTful-API/inc/transacties/delete.php
@@ -1,11 +1,15 @@
 <?php
-check_required_fields(["tr_id"]);
+if (!isset($_GET['id']) || empty($_GET['id'])) {
+    die('{"error":"Missing transaction ID","status":"fail"}');
+}
+
+$tr_id = $conn->real_escape_string($_GET['id']);
 
 if(!$stmt = $conn->prepare("delete from Transacties where tr_id = ?")){
 	die('{"error":"Prepared Statement failed on prepare","errNo":' . json_encode($conn -> errno) .',"mysqlError":' . json_encode($conn -> error) .',"status":"fail"}');
 }
 
-if(!$stmt -> bind_param("i", $postvars['tr_id'])){
+if(!$stmt -> bind_param("i", $tr_id)){
 	die('{"error":"Prepared Statement bind failed on bind","errNo":' . json_encode($conn -> errno) .',"mysqlError":' . json_encode($conn -> error) .',"status":"fail"}');
 }
 $stmt -> execute();
