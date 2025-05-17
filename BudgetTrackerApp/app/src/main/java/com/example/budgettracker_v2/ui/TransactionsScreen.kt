377ac12ad5a5b8954f69bc841a17a499cc052efa
Erhan99 <146257442+Erhan99@users.noreply.github.com>
2025-05-17 23:27:46 +0200

    UI improvements

--- BudgetTrackerApp/app/src/main/java/com/example/budgettracker_v2/ui/TransactionsScreen.kt
+++ BudgetTrackerApp/app/src/main/java/com/example/budgettracker_v2/ui/TransactionsScreen.kt
@@ -1,15 +1,18 @@
 package com.example.budgettracker_v2.ui
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
@@ -19,15 +22,20 @@ import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import com.example.budgettracker_v2.R
+import com.example.budgettracker_v2.models.Categorie
 import com.example.budgettracker_v2.models.Transaction
 import com.example.budgettracker_v2.viewmodels.TransactionViewModel
 import com.google.gson.Gson
+import kotlin.math.abs
 import kotlin.math.exp
 
 @Composable
@@ -76,79 +84,21 @@ fun TransactionScreen(navController: NavController, VM: TransactionViewModel = v
                     }
                 }
         }
-        TransactionList(transactions = items, listState, isLoading.value, navController)
-    }
-}
-
-@Composable
-fun TransactionCard(modifier: Modifier = Modifier, transaction: Transaction, navController: NavController){
-    val date = String.format("%s %s %s", transaction.dt_dag, transaction.dt_maand, transaction.dt_jaar)
-    val price = String.format("€ %s", transaction.tr_bedrag)
-    val expanded = remember { mutableStateOf(false) }
-    Card (modifier = modifier){
-        Column (modifier = Modifier.padding(16.dp)){
-            Row {
+        Column (
+            modifier = Modifier.fillMaxWidth()
+        ){
+            Row (
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center
+            ){
                 Text(
-                    text = transaction.tr_begunstigde,
-                    fontSize = 18.sp,
-                    fontWeight = FontWeight.Bold
-                )
-                Spacer(
-                    modifier = Modifier.weight(1f)
-                )
-                Text(
-                    text = price,
-                    fontSize = 18.sp,
+                    text = "Transactions",
+                    fontSize = 24.sp,
                     fontWeight = FontWeight.Bold,
-                    color = if(transaction.tr_bedrag < 0) Color(0xFFC62828) else Color(0xFF2E7D32)
+
                 )
-                Box {
-                    IconButton(onClick = { expanded.value = true }) {
-                        Icon(Icons.Default.MoreVert, contentDescription = "Menu")
-                    }
-                    DropdownMenu(
-                        expanded = expanded.value,
-                        onDismissRequest = { expanded.value = false }
-                    ) {
-                        DropdownMenuItem(
-                            onClick = {
-                                expanded.value = false
-                                val transactionJson = Gson().toJson(transaction)
-                                navController.navigate("transactionDetails/$transactionJson")
-                            },
-                            text = { Text("Details") }
-                        )
-                        DropdownMenuItem(
-                            onClick = {
-                                expanded.value = false
-                                navController.navigate("transactionEdit")
-                            },
-                            text = { Text("Bewerken") }
-                        )
-                        DropdownMenuItem(
-                            onClick = { expanded.value = false },
-                            text = { Text("Verwijderen") }
-                        )
-                    }
-                }
             }
-
-                Text(
-                    modifier = Modifier.padding(vertical = 3.dp),
-                    text = transaction.ct_naam ,
-                    fontSize = 12.sp,
-                )
-                Text(
-                    modifier = Modifier.padding(vertical = 3.dp),
-                    text = date,
-                    fontSize = 12.sp,
-                )
-                Text(
-                    modifier = Modifier.padding(vertical = 3.dp),
-                    text = transaction.tr_mededeling,
-                    fontSize = 12.sp,
-                )
-
+            TransactionList(transactions = items, listState, isLoading.value, navController)
         }
     }
 }
@@ -161,7 +111,7 @@ fun TransactionList(transactions: List<Transaction>?, state: LazyListState, isLo
         ){
             items(transactions){
                     transaction ->
-                TransactionCard(modifier = Modifier.padding(6.dp), transaction = transaction, navController = navController)
+                TransactionCard(modifier = Modifier.padding(8.dp), transaction = transaction, navController = navController)
             }
             if (isLoading) {
                 item {
@@ -179,5 +129,85 @@ fun TransactionList(transactions: List<Transaction>?, state: LazyListState, isLo
         Text(
             text = "no transactions"
         )
+    }
+}
+
+@Composable
+fun TransactionCard(modifier: Modifier = Modifier, transaction: Transaction, navController: NavController){
+    val date = String.format("%s %s %s", transaction.dt_dag, transaction.dt_maand.substring(0,3), transaction.dt_jaar)
+    val price = String.format("€ %s", transaction.tr_bedrag)
+    val expanded = remember { mutableStateOf(false) }
+    Card (modifier = modifier){
+        Column (modifier = Modifier.padding(16.dp).fillMaxWidth()){
+            Row (
+                modifier = Modifier.fillMaxWidth()
+            ){
+                Column (
+                    modifier = Modifier.weight(1f)
+                ){
+                    Text(
+                        text = transaction.tr_begunstigde,
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.Bold,
+
+                    )
+                    Text(
+                        modifier = Modifier.padding(vertical = 3.dp),
+                        text = transaction.ct_naam ,
+                        fontSize = 12.sp
+                    )
+                }
+                Column {
+                    Text(
+                        text = price,
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = if(transaction.tr_bedrag < 0) Color(0xFFC62828) else Color(0xFF2E7D32)
+                    )
+                    Text(
+                        modifier = Modifier.padding(vertical = 3.dp),
+                        text = date,
+                        fontSize = 12.sp,
+                        textAlign = TextAlign.Right
+                    )
+                }
+                TransactionDropDownMenu(expanded, transaction, navController)
+
+            }
+        }
+    }
+}
+
+
+@Composable
+fun TransactionDropDownMenu(expanded: MutableState<Boolean>, transaction: Transaction, navController: NavController){
+    Box{
+        IconButton(onClick = { expanded.value = true }) {
+            Icon(Icons.Default.MoreVert, contentDescription = "Menu")
+        }
+        DropdownMenu(
+            expanded = expanded.value,
+            onDismissRequest = { expanded.value = false }
+        ) {
+            DropdownMenuItem(
+                onClick = {
+                    expanded.value = false
+                    val transactionJson = Gson().toJson(transaction)
+                    navController.navigate("transactionDetails/$transactionJson")
+                },
+                text = { Text("Details") }
+            )
+            DropdownMenuItem(
+                onClick = {
+                    expanded.value = false
+                    navController.navigate("transactionEdit")
+                },
+                text = { Text("Bewerken") }
+            )
+            DropdownMenuItem(
+                onClick = { expanded.value = false },
+                text = { Text("Verwijderen") }
+            )
+        }
     }
 }
